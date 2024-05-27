3f28ffd1e06b902188a3b9aa609ee8266c4b47c4
Teddykavooh <teddykavooh@gmail.com>
2024-05-27 12:45:26 +0300

    Add sign-in persistence and UI necessities

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -3,11 +3,13 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import signIn from "../../firebase/auth/signin";
 import styles from "../page.module.css";
+import persistAuth from "@/firebase/auth/signinpersistence";
 
 function Page() {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const router = useRouter();
+    const [status, setStatus] = React.useState(false);
 
     const handleForm = async event => {
         event.preventDefault();
@@ -15,9 +17,21 @@ function Page() {
         if (myError) {
             return console.log(myError);
         }
-        console.log(result);
+        // console.log(result, status);
+        persistAuth(status);
         alert("Sign-In successfull");
         return router.push("/admin");
+    };
+
+    const handleCheckboxChange = (event) => {
+        const newStatus = event.target.checked;
+        setStatus(newStatus);
+        // console.log("Status should change to new: ", newStatus);
+        // console.log("Status1: ", status);
+        // console.log("Status2: ", status);
+        // console.log("Status3: ", status);
+        // console.log("Status4: ", status);
+        // console.log("Status5: ", status);
     };
 
     return (
@@ -50,6 +64,11 @@ function Page() {
                         />
                     </label>
                     <button type="submit" className={styles.submitButton}>SignIn</button>
+                    <label htmlFor="checker" className={styles.customCheckbox}>
+                        <input type="checkbox" className={styles.checkbox} id="checker" onChange={handleCheckboxChange} checked={status}/>
+                        <span className={styles.checkmark}></span>
+                        <p className={styles.labelText}>Remember me</p>
+                    </label>
                 </form>
             </div>
         </div>
